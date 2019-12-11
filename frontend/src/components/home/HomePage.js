cedd87cb61d3c8177ee3a783096fa3a5a1ec24f6
ShivaNikeghbali <shiva.nikeghbali@gmail.com>
2019-12-11 13:50:59 +0100

    About text added

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -1,44 +1,45 @@
 import React, { Component } from "react";
 
-class Checkbox extends React.Component {
-  state = { checked: false }
-  handleCheckboxChange = event =>
-    this.setState({ checked: event.target.checked })
-  render() {
+class HomePage extends React.Component {
+ render() {
     return (
-      <div>
-        <label>
-          <Checkbox
-            checked={this.state.checked}
-            onChange={this.handleCheckboxChange}
-          />
-          <span>Seller</span>
-        </label>
-       <label>
-        <Checkbox
-        checked={this.state.checked}
-        onChange={this.handleCheckboxChange}
-         />
-         <span>Buyer</span>
-         </label>
-         <label>
-        <Checkbox
-        checked={this.state.checked}
-        onChange={this.handleCheckboxChange}
-         />
-         <span>About</span>
-         </label>
-         <label>
-        <Checkbox
-        checked={this.state.checked}
-        onChange={this.handleCheckboxChange}
-         />
-         <span>Profile</span>
-         </label>       
+      <div className="About-us">
+        <h1>About us</h1>
+        <br />
+        <br />
+        <p>
+          <h4>What is Transparent Transaction?</h4>
+          TrustMe is a service especially designed for having more safety in
+          buying things from other people online. While it is easy and
+          definitely cheaper to buy used items online, it is not always a
+          success. One can end up buying an item that is not working properly
+          without having any insurance to return the item. This is where TrustMe
+          enters the picture. You can choose an item, agree on a price with the
+          seller and pay for it and have 2 days to test the item and make sure
+          it is in the condition the seller claimed it to be while we will keep
+          the money for this period of time. Upon your confirmation after two
+          days, the transaction will go forward and seller will receive the
+          money.
+          <br />
+          <br />
+          <h4> Why Transparent Transaction? </h4>
+          This transaction is based on trust. We trust in your good nature as an
+          honest and responsible individual who will use the 2 days time to
+          examine the item and not anything else.
+          <br />
+          <br />
+          <h4>How does it work?</h4>
+          As a user, you can buy and sell items. The seller uploads photos of
+          the item along with its description and price. Seller will receive a
+          link to the item page which they can share with buyers. Upon
+          agreement, the buyer and seller sign an online agreement and the
+          transaction will take place only to be withheld for a period of time
+          by us. In this time the buyer has the chance to review the item and
+          ask to return it if needed.
+        </p>
       </div>
-      
-    )
+    );
   }
 }
 
-export default Checkbox;
+export default HomePage;
